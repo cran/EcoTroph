9c37491871b7ed9fb83f1d03d7ec4d926540feb1
Jerome Guitton <jerome.guitton@agrocampus-ouest.fr>
2012-02-02 00:00:00 +0000

    version 1.2

---
+++ R/plot_Transpose.R
@@ -0,0 +1,43 @@
+plot_Transpose <-
+function(tab_Trans,scale=NULL,title=NULL)    #the scale parameter can be log or not for the y axis
+{                                                               
+
+
+scale <- if(is.null(scale)) '' else 'y'   
+opar=par(no.readonly=TRUE)
+par(mfrow=c(3,1))
+m <- (tab_Trans[-1,])
+m <- m[-c(32:51),]
+tmp_vecteur=m[,1]
+
+y_max<-max(apply(m,1,sum))
+y_min<-min(apply(m,1,sum))
+
+par(mar=c(5,5,1,15))
+
+plot(rownames(m),apply(m,1,sum),log=scale,col=2,type='l',axes=F,ylab="Biomasses",xlab="TL",main=title,ylim=c(y_min,y_max),xaxs = "i",xlim=c(2,5))
+
+m_cum <- apply(m,1,cumsum)
+
+for(i in length(rownames(m_cum)):1) 
+{
+polygon(c(2,as.numeric(colnames(m_cum)),5),c(y_min,m_cum[i,],y_min),col=i,xlim=c(2,5),border='NA')
+polygon(c(2,2,5,5),c(min(m_cum[i,]),y_min,y_min,min(m_cum[i,])),col='white',border='NA')
+}
+axis(4)
+legend(5.4,0.1,legend = colnames(m), bg = 'gray90',col=seq(1:ncol(m)),pch=1,xpd=NA)
+
+plot(rownames(m),tmp_vecteur,log=scale,col=1,type='l',axes=F,ylab="Biomasses",xlab="TL",ylim=c(y_min,y_max),xlim=c(2,5))
+axis(4)
+for (compteur in 2:ncol(m))
+{
+tmp_vecteur=tmp_vecteur+m[,compteur]
+lines(rownames(m),tmp_vecteur,col=compteur)
+}
+   
+plot(rownames(m),apply(m,1,sum),log=scale,col=2,type='l',bg='gray',axes=F,ylab="Biomasses",xlab="TL",ylim=c(y_min,y_max),xlim=c(2,5))
+axis(1)
+axis(4)
+par(opar)  
+}
+
